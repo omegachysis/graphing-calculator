fe430ded9364632cfbfe4d056cd3a562735d972f
Matthew Robinson <omegachysis@outlook.com>
2014-09-30 16:26:08 -0400

    fixed preprocessor

--- graphing-calculator/interpret.py
+++ graphing-calculator/interpret.py
@@ -6,9 +6,6 @@ def preprocessExpression(expression):
     """
     expression = str(expression)
 
-    # turn the whole expression into a large quantity
-    expression = "(" + expression + ")"
-
     # remove all whitespace
     expression = expression.replace(" ", "")
 
@@ -37,8 +34,15 @@ def preprocessExpression(expression):
             newExpression += "*1/"
     expression = newExpression
 
+    # turn the whole expression into a large quantity
+    expression = "(" + expression + ")"
+
     return expression
 
 def _test():
     test = preprocessExpression("5 - 4 * 8 + 90")
     assert test == "(5+-1*4*8+90)", "test failed: %s"%repr(test)
+    test = preprocessExpression("-9 * 50 - 100 / 5")
+    assert test == "(-1*9*50+-1*100*1/5)", "test failed: %s"%repr(test)
+
+    print("Tests succeeded!")
